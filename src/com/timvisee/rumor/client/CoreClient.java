8f5af0b097081458070170681152efdf0833e3e9
Tim Visée <timvisee@gmail.com>
2014-06-22 15:38:51 +0200

    Created a frame for test purposes
    Created a dummy frame for testing purposes.

--- src/com/timvisee/rumor/client/CoreClient.java
+++ src/com/timvisee/rumor/client/CoreClient.java
@@ -5,6 +5,10 @@ import com.timvisee.rumor.Defaults;
 import com.timvisee.rumor.util.Profiler;
 import com.timvisee.rumor.client.connection.server.ServerConnector;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
 public class CoreClient extends Core {
 
     /** Server connector instance */
@@ -18,31 +22,34 @@ public class CoreClient extends Core {
     }
 
     /**
-         * Initialize
-         * @return True on success, false on failure.
-         */
-        @Override
-        protected boolean init() {
-            // Profile the initialization
-            Profiler initProf = new Profiler(true);
+     * Initialize
+     * @return True on success, false on failure.
+     */
+    @Override
+    protected boolean init() {
+        // Profile the initialization
+        Profiler initProf = new Profiler(true);
 
-            // Show an initialization message
-            CoreClient.getLogger().info("Initializing " + Defaults.APP_CLIENT_NAME + " v" + Defaults.APP_VERSION_NAME + " (" + Defaults.APP_VERSION_CODE + ")...");
+        // Show an initialization message
+        CoreClient.getLogger().info("Initializing " + Defaults.APP_CLIENT_NAME + " v" + Defaults.APP_VERSION_NAME + " (" + Defaults.APP_VERSION_CODE + ")...");
 
-            // TODO: Initialization...
+        // TODO: Initialization...
 
-            // Stop the initialization profiler
-            initProf.stop();
+        // Stop the initialization profiler
+        initProf.stop();
 
-            // Initialization finished, show a message
-            CoreClient.getLogger().info("Successfully initialized, took " + initProf.getDurationString() + "! Cave Johnson here!");
+        // Initialization finished, show a message
+        CoreClient.getLogger().info("Successfully initialized, took " + initProf.getDurationString() + "! Cave Johnson here!");
 
-            // TODO: Perform a demo connection for test purposes
-            this.con = new ServerConnector("localhost");
-            this.con.connect();
+        // TODO: Perform a demo connection for test purposes
+        this.con = new ServerConnector("localhost");
+        this.con.connect();
 
-            // The initialization seems to be fine, return true
-            return true;
+        // Build a window for test purposes
+        buildTestWindow();
+
+        // The initialization seems to be fine, return true
+        return true;
     }
 
     /**
@@ -51,5 +58,37 @@ public class CoreClient extends Core {
      */
     public ServerConnector getServerConnector() {
         return this.con;
+    }
+
+
+
+    public void buildTestWindow() {
+        Random rand = new Random();
+
+        // Create the frame and set it's properties
+        JFrame f = new JFrame(Defaults.APP_CLIENT_NAME + " v" + Defaults.APP_VERSION_NAME);
+        f.setLayout(new FlowLayout());
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.setMinimumSize(new Dimension(600, 400));
+
+        // Create a list with some dummy text
+        DefaultListModel dlm = new DefaultListModel();
+        for(int i3 = 0; i3 < 100; i3++)
+            dlm.addElement("User: " + rand.nextInt(Integer.MAX_VALUE));
+        JList list = new JList(dlm);
+
+        // Create the chat view
+        JScrollPane chatView = new JScrollPane(list);
+        chatView.setPreferredSize(new Dimension(300, 200));
+        f.add(chatView);
+
+        // Pack all the components in the frame
+        f.pack();
+
+        // Set the frame's location
+        f.setLocationRelativeTo(null);
+
+        // Show the frame
+        f.setVisible(true);
     }
 }
